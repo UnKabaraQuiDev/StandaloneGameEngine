6b14f3c424605f78f92e26b7498468ec41dd9389
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2025-12-11 08:50:52 +0100

    removed shader instantiation exception é

--- src/main/java/lu/kbra/standalone/gameengine/exceptions/opengl/ShaderInstantiationException.java
+++
@@ -1,9 +0,0 @@
-package lu.kbra.standalone.gameengine.exceptions.opengl;
-
-public class ShaderInstantiationException extends RuntimeException {
-
-	public ShaderInstantiationException(Exception e) {
-		super(e);
-	}
-
-}
